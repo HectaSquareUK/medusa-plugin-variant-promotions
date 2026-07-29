0f97e64c12ec65b7f58fc7187c061a0213ae56e5
HectaSquareUk <support@videonprompt.com>
2026-07-29 22:38:28 +0500

    fix: export required widgetModule, routeModule, and menuItemModule in admin/index.js

--- admin/index.js
+++ admin/index.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const widgetModule = { widgets: [] };
+const routeModule = { routes: [] };
+const menuItemModule = { menuItems: [] };
+const formModule = { customFields: {} };
+const displayModule = { displays: {} };
+const i18nModule = { resources: {} };
+const layoutModule = { layouts: [] };
+
+const plugin = {
+  widgetModule,
+  routeModule,
+  menuItemModule,
+  formModule,
+  displayModule,
+  i18nModule,
+  layoutModule
+};
+
+module.exports = plugin;
